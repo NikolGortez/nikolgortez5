75be6f5d15b28a6b3a30f1f803da806abfea7b8f
Nikol Gortez <117678971+NikolGortez@users.noreply.github.com>
2022-12-05 22:39:17 +0300

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,51 +1,25 @@
-function click1()
-{
-	let price = document.getElementsByName("price");
-    let count = document.getElementsByName("count");
-    let result = document.getElementById("result");
-	let toPrice = price[
-        0
-    ].value;
-	let toCount = count[
-        0
-    ].value;
-	let flag1 = toCount.match(/^\d+$/);
-	let flag2 = toPrice.match(/^\d+$/);
-	if(flag2 !== null && flag1 !== null)
-	{
-		result.innerHTML = parseInt(price[
-            0
-        ].value)*parseInt(count[
-            0
-        ].value);
-    }
-	else
-	if(flag2 === null && flag1 !== null)
-	{
-		result.innerHTML = "Введите число в поле цена";
-    }
-	else
-	if(flag1 === null && flag2 !== null)
-	{
-		result.innerHTML = "Введите число в поле количество";
-    }
-	else
-	if(flag2 === null && flag1 === null)
-	{
-		result.innerHTML = "Введите число в оба поля";
-    }
-	return false;
-}
+window.addEventListener("DOMContentLoaded", function (event) 
+    {
+        console.log("DOM fully loaded and parsed");
+        function calculate() 
+        {
+            var count = document.getElementById("field1").value;
+            var cost = document.getElementById("field2").value;
+            var res = document.getElementById("result");
 
-function click();
-{
-	alert("Работа завершена");
-        function click1();
-}
-
-window.addEventListener("DOMContentLoaded", function (event)
-{
-	console.log("DOM fully loaded and parsed");
-	let b = document.getElementById("calculateResult");
-	b.addEventListener("click", onClick);
-});
+            var er=/^(?!(0))\d+$/;
+            if(count.match(er)===null || cost.match(er)===null)
+            {
+                alert("Вы ввели неподходящие значения");
+                res.innerHTML="Ошибка";
+            }
+            else
+            {
+                res.innerHTML = cost*count;
+            }
+        
+        }
+        var b = document.getElementById("button1");
+        b.addEventListener("click", calculate);
+    }
+);
